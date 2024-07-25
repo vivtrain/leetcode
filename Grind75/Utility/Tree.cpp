fce1faccb24d03defb87729556cf3dad1a9709af
Vivek Kumar <k99vivek@gmail.com>
2024-07-24 19:40:44 -0700

    Moved Utility/Tree tools to namespace BST

--- Grind75/Utility/Tree.cpp
+++ Grind75/Utility/Tree.cpp
@@ -1,43 +1,49 @@
 #include "Tree.h"
 
-void freeTree(TreeNode *root) {
-  if (root) {
-    freeTree(root->left);
-    freeTree(root->right);
-    free(root);
-  }
-}
+namespace BST {
 
-void insertTree(TreeNode *root, int val) {
-  if (root) {
-    if (val < root->val) {
-      if (root->left)
-        insertTree(root->left, val);
-      else
-        root->left = new TreeNode(val);
-    } else if (val > root->val) {
-      if (root->right)
-        insertTree(root->right, val);
-      else
-        root->right = new TreeNode(val);
+  void freeTree(BST::TreeNode *root) {
+    if (root) {
+      freeTree(root->left);
+      freeTree(root->right);
+      free(root);
     }
   }
-}
 
-TreeNode* makeTree(std::vector<int> ints) {
-  if (ints.empty())
-    return nullptr;
-  TreeNode *root = new TreeNode(ints.front());
-  for (size_t index = 1; index < ints.size(); index++)
-    insertTree(root, ints[index]);
-  return root;
-}
+  void insertTree(TreeNode *root, int val) {
+    if (root) {
+      if (val < root->val) {
+        if (root->left)
+          insertTree(root->left, val);
+        else
+          root->left = new TreeNode(val);
+      } else if (val > root->val) {
+        if (root->right)
+          insertTree(root->right, val);
+        else
+          root->right = new TreeNode(val);
+      }
+    }
+  }
 
-void printTree(std::string prefix, TreeNode *root, bool isLeft) {
-  if (root) {
-    std::cout << prefix << (isLeft ? "|--" : "`--") << root->val << std::endl;
-    printTree(prefix + (isLeft ? "|  " : "   "), root->left, true);
-    printTree(prefix + (isLeft ? "|  " : "   "), root->right, false);
+  TreeNode* makeTree(std::vector<int> ints) {
+    if (ints.empty())
+      return nullptr;
+    TreeNode *root = new TreeNode(ints.front());
+    for (size_t index = 1; index < ints.size(); index++)
+      insertTree(root, ints[index]);
+    return root;
+  }
+
+  void printTree_recurse(std::string prefix, TreeNode *root, bool isLeft) {
+    if (root) {
+      std::cout << prefix << (isLeft ? "|--" : "`--") << root->val << std::endl;
+      printTree_recurse(prefix + (isLeft ? "|  " : "   "), root->left, true);
+      printTree_recurse(prefix + (isLeft ? "|  " : "   "), root->right, false);
+    }
+  }
+  void printTree(TreeNode *root) {
+    printTree_recurse("", root, false);
   }
 }
 
--- Grind75/Utility/Tree.h
+++ Grind75/Utility/Tree.h
@@ -5,19 +5,24 @@
 #include <string>
 #include <iostream>
 
-struct TreeNode {
-  int val;
-  TreeNode *left;
-  TreeNode *right;
-  TreeNode() : val(0), left(nullptr), right(nullptr) {}
-  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+namespace BST {
 
-void freeTree(TreeNode *root);
-void insertTree(TreeNode *root, int val);
-TreeNode* makeTree(std::vector<int> ints);
-void printTree(std::string prefix, TreeNode *root, bool isLeft);
+  struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+  void freeTree(TreeNode *root);
+  void insertTree(TreeNode *root, int val);
+  TreeNode* makeTree(std::vector<int> ints);
+  void printTree_recurse(std::string prefix, TreeNode *root, bool isLeft);
+  void printTree(TreeNode *root);
+
+}
 
 #endif
 
