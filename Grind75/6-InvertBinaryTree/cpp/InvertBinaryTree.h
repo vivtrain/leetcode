9611e1b59c33d0cf6576b8a28f45e2063957088e
Vivek Kumar <k99vivek@gmail.com>
2024-07-24 19:11:28 -0700

    Update 6-InvertBinaryTree to use Utility/Tree.h

--- Grind75/6-InvertBinaryTree/cpp/InvertBinaryTree.h
+++ Grind75/6-InvertBinaryTree/cpp/InvertBinaryTree.h
@@ -1,14 +1,7 @@
 #ifndef INVERT_BINARY_TREE
 #define INVERT_BINARY_TREE
 
- struct TreeNode {
-   int val;
-   TreeNode *left;
-   TreeNode *right;
-   TreeNode() : val(0), left(nullptr), right(nullptr) {}
-   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- };
+#include "../../Utility/Tree.h"
 
 class InvertBinaryTree {
   public:
--- Grind75/6-InvertBinaryTree/cpp/test.cpp
+++ Grind75/6-InvertBinaryTree/cpp/test.cpp
@@ -2,47 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
-void freeTree(TreeNode *root) {
-  if (root) {
-    freeTree(root->left);
-    freeTree(root->right);
-    free(root);
-  }
-}
-
-void insert(TreeNode *root, int val) {
-  if (root) {
-    if (val < root->val) {
-      if (root->left)
-        insert(root->left, val);
-      else
-        root->left = new TreeNode(val);
-    } else if (val > root->val) {
-      if (root->right)
-        insert(root->right, val);
-      else
-        root->right = new TreeNode(val);
-    }
-  }
-}
-
-TreeNode* makeTree(std::vector<int> ints) {
-  if (ints.empty())
-    return nullptr;
-  TreeNode *root = new TreeNode(ints.front());
-  for (size_t index = 1; index < ints.size(); index++)
-    insert(root, ints[index]);
-  return root;
-}
-
-void printTree(std::string prefix, TreeNode *root, bool isLeft) {
-  if (root) {
-    std::cout << prefix << (isLeft ? "|--" : "`--") << root->val << std::endl;
-    printTree(prefix + (isLeft ? "|  " : "   "), root->left, true);
-    printTree(prefix + (isLeft ? "|  " : "   "), root->right, false);
-  }
-}
+#include "../../Utility/Tree.h"
 
 int main() {
   InvertBinaryTree inv;
