90008635ca6145d7d3faf58e28fa8efa955c654d
Vivek Kumar <k99vivek@gmail.com>
2024-08-07 11:18:40 -0700

    Completed 12-LinkedListCycle/python

--- Grind75/12-LinkedListCycle/python/LinkedListCycle.py
+++ Grind75/12-LinkedListCycle/python/LinkedListCycle.py
@@ -3,15 +3,35 @@
 from __future__ import annotations
 import sys
 sys.path.append('../../Utility/python')
+from List import *
+from typing import Optional
 
-class Solution(object):
-  pass
+class Solution:
+  def hasCycle(self, head: Optional[ListNode]) -> bool:
+    if head is None:
+      return False
+    slow = head
+    fast = head.next
+    while fast is not None:
+      if slow is fast:
+        return True
+      slow = slow.next
+      fast = fast.next
+      if fast is None:
+        break
+      fast = fast.next
+    return False
 
 if __name__ == '__main__':
   soln = Solution()
   testCases = []
+
+  lst = makeList([1,2,3])
+  print(soln.hasCycle(lst))
+  lst.next.next.next = lst
+  print(soln.hasCycle(lst))
   
   for test in testCases:
     pass
-  print('TESTS PASSED!')
+  #print('TESTS PASSED!')
 
