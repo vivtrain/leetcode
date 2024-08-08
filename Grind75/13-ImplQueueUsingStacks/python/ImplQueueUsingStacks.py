a81ebe38ecafda9ee1afc893a17b509912d024a6
Vivek Kumar <k99vivek@gmail.com>
2024-08-08 14:24:58 -0700

    Completed 13-ImplQueueUsingStacks/python

--- Grind75/13-ImplQueueUsingStacks/python/ImplQueueUsingStacks.py
+++ Grind75/13-ImplQueueUsingStacks/python/ImplQueueUsingStacks.py
@@ -4,8 +4,32 @@ from __future__ import annotations
 import sys
 sys.path.append('../../Utility/python')
 
-class Solution(object):
-  pass
+class MyQueue(Object):
+  enqueue: list 
+  dequeue: list
+
+  def __init__(self):
+    self.enqueue = []
+    self.dequeue = []
+
+  def push(self, x: int) -> None:
+    self.enqueue.append(x)
+
+  def pop(self) -> int:
+    if len(self.dequeue) == 0:
+      while len(self.enqueue) > 0:
+        self.dequeue.append(self.enqueue.pop())
+    return self.dequeue.pop()
+
+  def peek(self) -> int:
+    if len(self.dequeue) == 0:
+      while len(self.enqueue) > 0:
+        self.dequeue.append(self.enqueue.pop())
+    return self.dequeue[-1]
+
+  def empty(self) -> bool:
+    return len(self.enqueue) == 0 and len(self.dequeue) == 0
+
 
 if __name__ == '__main__':
   soln = Solution()
@@ -13,5 +37,5 @@ if __name__ == '__main__':
   
   for test in testCases:
     pass
-  print('TESTS PASSED!')
+  #print('TESTS PASSED!')
 
