50dbc461c9467a7544944dc1123df52af34a4ec0
Vivek Kumar <k99vivek@gmail.com>
2024-08-11 18:39:51 -0700

    Completed 14-FirstBadVersion/python

--- Grind75/14-FirstBadVersion/python/FirstBadVersion.py
+++ Grind75/14-FirstBadVersion/python/FirstBadVersion.py
@@ -5,13 +5,39 @@ import sys
 sys.path.append('../../Utility/python')
 
 class Solution(object):
-  pass
+  bad: int
+  n: int
+
+  def isBadVersion(self, version: int) -> bool:
+    assert version >= 1
+    assert version <= self.n
+    return version >= self.bad
+
+  def firstBadVersion(self, n: int) -> int:
+    low = 1
+    high = n
+    while low < high:
+      mid = low + (high-low)//2
+      if self.isBadVersion(mid):
+        high = mid
+      else:
+        low = mid+1
+    return low
 
 if __name__ == '__main__':
   soln = Solution()
-  testCases = []
-  
-  for test in testCases:
-    pass
+
+  N = 10
+  for n in range(1, N+1):
+    soln.n = n
+    print(list(range(1, n+1)))
+    for bad in range(1,n+1):
+      soln.bad = bad
+      print(f'bad should be {bad} ', end='')
+      result = soln.firstBadVersion(n)
+      print(f'got {result}')
+      assert result == bad
+    print('')
+
   print('TESTS PASSED!')
 
