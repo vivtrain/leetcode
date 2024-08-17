4ec48d2207743bc507b6b45954a1b5dda84cd693
Vivek Kumar <k99vivek@gmail.com>
2024-08-16 17:36:43 -0700

    Completed 19-MajorityElement/python

--- Grind75/19-MajorityElement/python/MajorityElement.py
+++ Grind75/19-MajorityElement/python/MajorityElement.py
@@ -3,15 +3,40 @@
 from __future__ import annotations
 import sys
 sys.path.append('../../Utility/python')
+from collections import Counter
 
 class Solution(object):
-  pass
+  def majorityElement(self, nums: list[int]) -> int:
+    majorityNum = nums[0]
+    majorityCount = 0
+    for num in nums:
+      if num == majorityNum:
+        majorityCount += 1
+      else:
+        majorityCount -= 1
+        if majorityCount < 0:
+          majorityNum = num
+          majorityCount = 1
+    return majorityNum
+
+  def majorityElement2(self, nums: list[int]) -> int:
+    numCounts = Counter(nums)
+    return numCounts.most_common()[0][0]
+
 
 if __name__ == '__main__':
   soln = Solution()
-  testCases = []
+  testCases = [
+      ([1], 1),
+      ([1,1,2], 1),
+      ([1,2,2], 2),
+      ([1,2,2,1,1], 1),
+      ]
 
   for test in testCases:
-    pass
+    print(f'{test[0]}, {test[1]} should be majority, got ', end='')
+    result = soln.majorityElement2(test[0])
+    print(result)
+    assert result == test[1]
   print('TESTS PASSED!')
 
