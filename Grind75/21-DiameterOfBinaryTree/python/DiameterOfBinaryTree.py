b49a6f77ad592d485aecb224c13dff642bf5a9d2
Vivek Kumar <k99vivek@gmail.com>
2024-08-20 14:19:03 -0700

    Completed 21-DiameterOfBinaryTree/python

--- Grind75/21-DiameterOfBinaryTree/python/DiameterOfBinaryTree.py
+++ Grind75/21-DiameterOfBinaryTree/python/DiameterOfBinaryTree.py
@@ -3,15 +3,42 @@
 from __future__ import annotations
 import sys
 sys.path.append('../../Utility/python')
+from typing import Optional
+from Tree import *
 
 class Solution(object):
-  pass
+  def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+    def diameter_recurse(root: Optional[TreeNode], best: int)\
+        -> (int, int):
+      if root is None:
+        return (0, best)
+      leftDepth, best = diameter_recurse(root.left, best)
+      rightDepth, best = diameter_recurse(root.right, best)
+      best = max(leftDepth + rightDepth, best)
+      return max(leftDepth, rightDepth)+1, best
+
+    depth, diameter = diameter_recurse(root, 0)
+    return diameter
 
 if __name__ == '__main__':
   soln = Solution()
-  testCases = []
+  testCases = [
+      ([], 0),
+      ([1], 0),
+      ([2,1], 1),
+      ([2,1,3], 2),
+      ([2,1,3,4], 3),
+      ([1,4,3,5,2,6], 4),
+      ]
 
   for test in testCases:
-    pass
+    tree = makeTree(test[0])
+    diameter = test[1]
+    printTree(tree)
+    print(f'should have diameter {diameter}, got ', end='')
+    result = soln.diameterOfBinaryTree(tree)
+    print(result)
+    assert result == diameter
   print('TESTS PASSED!')
 
