a3cb6ca2a6e11d049337a3e5acb36253aba596a6
Vivek Kumar <k99vivek@gmail.com>
2024-08-22 15:07:14 -0700

    Completed 22-MiddleOfLinkedList/python

--- Grind75/22-MiddleOfLinkedList/python/MiddleOfLinkedList.py
+++ Grind75/22-MiddleOfLinkedList/python/MiddleOfLinkedList.py
@@ -3,15 +3,32 @@
 from __future__ import annotations
 import sys
 sys.path.append('../../Utility/python')
+from typing import Optional
+from List import *
 
 class Solution(object):
-  pass
+  def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    slow = head
+    fast = head
+    while fast is not None and fast.next is not None:
+      slow = slow.next
+      fast = fast.next.next
+    return slow
 
 if __name__ == '__main__':
   soln = Solution()
-  testCases = []
+  testCases = [(makeList([]), None)]
 
-  for test in testCases:
-    pass
+  for length in range(1,11):
+    lst = makeList(list(range(length)))
+    mid = atIndex(lst, length//2)
+    testCases.append((lst, mid))
+
+  for lst, mid in testCases:
+    print(strList(lst), 'mid at', mid.val if mid is not None else None, end='',
+        flush=True)
+    result = soln.middleNode(lst)
+    print(', got', result.val if result is not None else None)
+    assert result is mid
   print('TESTS PASSED!')
 
