cda3dfb68dc90e84a7d7a203eb08e4e06977db10
Vivek Kumar <k99vivek@gmail.com>
2024-08-22 15:52:20 -0700

    Fixed memory leak in 11-BalancedBinaryTree/cpp + Refactor test

--- Grind75/11-BalancedBinaryTree/cpp/test.cpp
+++ Grind75/11-BalancedBinaryTree/cpp/test.cpp
@@ -5,51 +5,42 @@
 #include <vector>
 #include <tuple>
 #include <prettyPrint.h>
-#include <chrono>
+#include <Time.h>
 
 int main() {
   BalancedBinaryTree soln;
-  std::vector<std::pair<BST::TreeNode*, bool>> testCases;
-  testCases.push_back(std::make_pair(BST::makeTree({}),              true));
-  testCases.push_back(std::make_pair(BST::makeTree({4}),             true));
-  testCases.push_back(std::make_pair(BST::makeTree({4,2}),           true));
-  testCases.push_back(std::make_pair(BST::makeTree({4,2,6}),         true));
-  testCases.push_back(std::make_pair(BST::makeTree({4,2,1}),         false));
-  testCases.push_back(std::make_pair(BST::makeTree({4,2,6,1}),       true));
-  testCases.push_back(std::make_pair(BST::makeTree({4,2,6,1,3}),     true));
-  testCases.push_back(std::make_pair(BST::makeTree({4,2,6,1,3,0}),   false));
-  testCases.push_back(std::make_pair(BST::makeTree({4,2,6,1,3,0,5}), true));
-  testCases.push_back(std::make_pair(BST::makeTree({4,2,6,5,7,9,8}), false));
-  testCases.push_back(std::make_pair(BST::makeTree({4,3,2,1,5,6,7}), false));
+  std::vector<std::pair<std::vector<int>, bool>> testCases {
+    {{},              true},
+    {{4},             true},
+    {{4,2},           true},
+    {{4,2,6},         true},
+    {{4,2,1},         false},
+    {{4,2,6,1},       true},
+    {{4,2,6,1,3},     true},
+    {{4,2,6,1,3,0},   false},
+    {{4,2,6,1,3,0,5}, true},
+    {{4,2,6,5,7,9,8}, false},
+    {{4,3,2,1,5,6,7}, false},
+  };
 
   for (auto test : testCases) {
-    BST::printTree(test.first);
-    std::cout << "should be " << std::boolalpha << test.second<<"\n"<<std::endl;
-    assert(soln.isBalanced(test.first) == test.second);
+    BST::TreeNode *tree = BST::makeTree(test.first);
+    BST::printTree(tree);
+    std::cout << "should be " <<std::boolalpha<< test.second<<", "<<std::flush;
+    bool result = soln.isBalanced2(tree);
+    std::cout << "got " << std::boolalpha << result << std::endl << std::endl;
+    BST::freeTree(tree);
+    assert(result == test.second);
   }
   std::cout << "TESTS PASSED!" << std::endl;
 
-  size_t N = 1, exp = 25, levels = 0, l = 1, t = 0;
-  for (size_t i = 0; i < exp; i++)
-    N += N;
-  for (size_t n = N; n > 0; n /= 2) {
-    levels++;
-  }
-  std::vector<int> tree(N-1);
-  for (size_t i = 2; i < levels+1; i++) {
-    l += l;
-    for (size_t n = N/l; n < N; n+=N/l*2) {
-      tree[t] = n;
-      t++;
-    }
-  }
-  std::cout << "Stress testing with N = " << N << " elements..." << std::endl;
+  std::cout << "Stress testing..." << std::endl;
+  BST::TreeNode *tree = BST::bushy(20);
 
-  auto start = std::chrono::high_resolution_clock::now();
-  soln.isBalanced(BST::makeTree(tree));
-  auto stop = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>
-    (stop - start);
-  std::cout << "Took " << duration.count()/1000. << " seconds" << std::endl;
+  auto start = Time::now();
+  soln.isBalanced(tree);
+  auto elapsed = Time::elapsed(start);
+  std::cout << "Took " << elapsed << " seconds" << std::endl;
+  BST::freeTree(tree);
 }
 
