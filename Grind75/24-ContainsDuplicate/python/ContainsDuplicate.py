9cc931d288b53c95b33dffc4c6f5708da9a4da59
Vivek Kumar <k99vivek@gmail.com>
2024-08-26 22:58:56 -0700

    Completed 24-ContainsDuplicate/python

--- Grind75/24-ContainsDuplicate/python/ContainsDuplicate.py
+++ Grind75/24-ContainsDuplicate/python/ContainsDuplicate.py
@@ -5,13 +5,33 @@ import sys
 sys.path.append('../../Utility/python')
 
 class Solution(object):
-  pass
+  def containsDuplicate(self, nums: list[int]) -> bool:
+    return len(set(nums)) != len(nums)
+
+  def containsDuplicate2(self, nums: list[int]) -> bool:
+    s = set()
+    for n in nums:
+      if n in s:
+        return True
+      else:
+        s.add(n)
+    return False
 
 if __name__ == '__main__':
   soln = Solution()
-  testCases = []
+  testCases: list[tuple[list[int], bool]] = [
+    ([], False),
+    ([1], False),
+    ([1,2], False),
+    ([1,2,3], False),
+    ([1,2,2], True),
+    ([1,2,2,1], True),
+  ]
 
-  for test in testCases:
-    pass
+  for lst, hasDup in testCases:
+    print(f'{lst} does{" not" if not hasDup else ""} have a duplicate, ', end='')
+    res = soln.containsDuplicate2(lst)
+    print(res)
+    assert res == hasDup
   print('TESTS PASSED!')
 
