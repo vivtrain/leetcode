93004c832695281f45933acc1108ae25aa3b650b
Vivek Kumar <k99vivek@gmail.com>
2024-08-26 21:18:37 -0700

    Completed 23-MaxDepth/python

--- Grind75/23-MaxDepth/python/MaxDepth.py
+++ Grind75/23-MaxDepth/python/MaxDepth.py
@@ -3,15 +3,32 @@
 from __future__ import annotations
 import sys
 sys.path.append('../../Utility/python')
+from Tree import *
 
 class Solution(object):
-  pass
+  def maxDepth(self, root: Optional[TreeNode]) -> int:
+    if root is None:
+      return 0
+    return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
 
 if __name__ == '__main__':
   soln = Solution()
-  testCases = []
+  testCases: list[tuple[list, int]] = [
+    ([], 0),
+    ([1], 1),
+    ([2,1], 2),
+    ([2,1,3], 2),
+    ([2,1,3,4], 3),
+    ([2,1,7,5,4,3,6], 5),
+  ]
 
-  for test in testCases:
-    pass
+  for lst, depth in testCases:
+    tree = makeTree(lst)
+    printTree(tree)
+    print(f'should have a depth of {depth}, ', end='')
+    res = soln.maxDepth(tree)
+    print(f'got {res}')
+    assert res == depth
   print('TESTS PASSED!')
 
