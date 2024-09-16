227894a38d69361ce2ed45e3481e240ddc7b5c29
Vivek Kumar <k99vivek@gmail.com>
2024-09-15 17:02:51 -0700

    Pretty print 2D vectors

--- Grind75/Utility/cpp/include/prettyPrint.h
+++ Grind75/Utility/cpp/include/prettyPrint.h
@@ -8,22 +8,35 @@
 
 template<class VecType>
 void prettyPrint(const std::vector<VecType>& vec) {
-  std::cout << "[ ";
-  for (auto it = vec.begin(); it != vec.end(); it++) {
-    std::cout << *it << ' ';
-  }
-  std::cout << ']' << std::endl;
+  std::cout << '[';
+  for (auto it = vec.begin(); it != vec.end()-1; it++)
+    std::cout << *it << ", ";
+  std::cout << *(vec.end()-1) << ']' << std::endl;
 }
 
 template<class VecType>
 std::string prettyString(const std::vector<VecType>& vec) {
   std::stringstream output;
-  output << "[ ";
-  for (auto it = vec.begin(); it != vec.end(); it++) {
-    output << *it << ' ';
-  }
-  output << ']';
+  output << '[';
+  for (auto it = vec.begin(); it != vec.end()-1; it++)
+    output << *it << ", ";
+  output << *(vec.end()-1) << ']';
   return output.str();
+}
+
+template<class VecType>
+void prettyPrint2D(const std::vector<std::vector<VecType>>& vec) {
+  std::cout << "[";
+  if (vec.size() > 0)
+    std::cout << prettyString(*vec.begin());
+  if (vec.size() > 1)
+    std::cout << std::endl;
+  if (vec.size() > 2)
+    for (auto it = vec.begin()+1; it != vec.end()-1; it++)
+      std::cout << " " << prettyString(*it) << '\n';
+  if (vec.size() > 1)
+    std::cout << " " << prettyString(*(vec.end()-1));
+  std::cout << "]" << std::endl;
 }
 
 #endif
