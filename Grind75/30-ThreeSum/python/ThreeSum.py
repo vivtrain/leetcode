3a5365eb38c03e40007fc1ff7660044d6ad45ce8
Vivek Kumar <k99vivek@gmail.com>
2025-05-12 13:49:53 -0700

    Completed 30-ThreeSum/python

--- Grind75/30-ThreeSum/python/ThreeSum.py
+++ Grind75/30-ThreeSum/python/ThreeSum.py
@@ -1,17 +1,47 @@
 #!/bin/python3
 
-from __future__ import annotations
-import sys
-sys.path.append('../../Utility/python')
+from sys import path
+path.append("../../Utility/python/")
+
+from typing import List
 
 class Solution(object):
-  pass
+  def threeSum(self, nums: List[int]) -> List[List[int]]:
+    if (len(nums) < 3):
+      return []
+    nums.sort()
+    result: List[List[int]] = []
+    for i in range(len(nums)):
+      if i > 0 and nums[i] == nums[i-1]:
+        continue
+      l, r = i+1, len(nums)-1
+      while l < r:
+        if l > i+1 and nums[l-1] == nums[l]:
+          l += 1
+          continue
+        threeSum = nums[i] + nums[l] + nums[r]
+        if threeSum < 0:
+          l += 1
+        elif threeSum > 0:
+          r -= 1
+        else:
+          result.append([nums[i], nums[l], nums[r]])
+          l += 1
+    return result
 
 if __name__ == '__main__':
   soln = Solution()
-  testCases = []
+  testCases = [
+    [  ],
+    [ 0, 0, 0 ],
+    [ -1, 0, 1 ],
+    [ 0, 0, 0, 0 ],
+    [ -1, 0, 1, 0 ],
+    [ 0, 0, 0, 0, 0 ],
+    [ -1, 0, 1, 2, -1, -4 ],
+    [ 0, -1, 1, -2, 2, 3, -4 ],
+  ]
 
   for test in testCases:
-    pass
-  print('TESTS PASSED!')
+    print(test, '->', soln.threeSum(test))
 
