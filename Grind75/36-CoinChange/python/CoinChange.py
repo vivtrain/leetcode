ef8d5b9ecc2dcb9d4cbfc6af228c968a8c9c1478
Vivek Kumar <k99vivek@gmail.com>
2025-06-05 17:06:01 -0700

    Added alternates to 36-CoinChange/python

--- Grind75/36-CoinChange/python/CoinChange.py
+++ Grind75/36-CoinChange/python/CoinChange.py
@@ -1,9 +1,10 @@
 #!/bin/python3
 
 from __future__ import annotations
+from collections import defaultdict
 import sys
 sys.path.append('../../Utility/python')
-from typing import Dict, List
+from typing import DefaultDict, Dict, List, Optional
 
 class Solution(object):
   def coinChange(self, coins: List[int], amount: int) -> int:
@@ -21,10 +22,42 @@ class Solution(object):
         lookupTable.append(-1)
     return lookupTable[amount]
 
+  def coinChange2(self, coins: List[int], amount: int) -> Optional[List[int]]:
+    cache: List[Optional[List[int]]] = [None] * (amount + 1)
+    cache[0] = []
+    for amt in range(1, amount+1):
+      for denom in coins:
+        leftover = cache[amt - denom] if amt - denom >= 0 else None
+        cached = cache[amt]
+        if leftover is not None \
+            and (cached is None or len(leftover) + 1 < len(cached)):
+          toInsert = leftover.copy()
+          toInsert.append(denom)
+          cache[amt] = toInsert
+    return cache[amount]
+
+  def coinChange3(self, coins: List[int], amount: int) -> Optional[DefaultDict[int,int]]:
+    cache: List[Optional[DefaultDict[int,int]]] = [None] * (amount + 1)
+    cache[0] = defaultdict(lambda: 0)
+    def countCoins(coinMap: DefaultDict[int,int]):
+      return sum(coinMap.values())
+    for amt in range(1, amount+1):
+      for denom in coins:
+        leftover = cache[amt - denom] if amt - denom >= 0 else None
+        cached = cache[amt]
+        if leftover is not None \
+            and (cached is None or countCoins(leftover) + 1 < countCoins(cached)):
+          toInsert = leftover.copy()
+          toInsert[denom] += 1
+          cache[amt] = toInsert
+    return cache[amount]
+
 if __name__ == '__main__':
   soln = Solution()
   coins = [3,5,7]
   print(coins)
   for amount in range(25):
-    print("amount: ",amount,", coins needed: ",soln.coinChange(coins, amount),sep="")
+    result =  soln.coinChange3(coins, amount)
+    result = dict(result) if result is not None else None
+    print("amount: ",amount,", coins needed: ",result,sep="")
 
