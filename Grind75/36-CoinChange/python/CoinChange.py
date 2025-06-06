6de5de66870bb4ccfbbc86a64c2c292029ade640
Vivek Kumar <k99vivek@gmail.com>
2025-06-05 23:31:34 -0700

    Added alternate Grind75/python

--- Grind75/36-CoinChange/python/CoinChange.py
+++ Grind75/36-CoinChange/python/CoinChange.py
@@ -36,7 +36,7 @@ class Solution(object):
           cache[amt] = toInsert
     return cache[amount]
 
-  def coinChange3(self, coins: List[int], amount: int) -> Optional[DefaultDict[int,int]]:
+  def coinChange3(self, coins: List[int], amount: int) -> Optional[Dict[int,int]]:
     cache: List[Optional[DefaultDict[int,int]]] = [None] * (amount + 1)
     cache[0] = defaultdict(lambda: 0)
     def countCoins(coinMap: DefaultDict[int,int]):
@@ -50,14 +50,43 @@ class Solution(object):
           toInsert = leftover.copy()
           toInsert[denom] += 1
           cache[amt] = toInsert
-    return cache[amount]
+    result = cache[amount]
+    return dict(result) if result is not None else None
+  
+  def coinChange4(self, coins: Dict[int,int], amount: int) -> Optional[Dict[int,int]]:
+    cache: List[Optional[DefaultDict[int,int]]] = [None] * (amount + 1)
+    cache[0] = defaultdict(lambda: 0)
+    def countCoins(coinMap: DefaultDict[int,int]):
+      return sum(coinMap.values())
+    for amt in range(1, amount+1):
+      for denom in coins:
+        leftover = cache[amt - denom] if amt - denom >= 0 else None
+        cached = cache[amt]
+        if leftover is not None \
+            and (cached is None or countCoins(leftover) + 1 < countCoins(cached)) \
+            and (leftover[denom] < coins[denom]):
+          toInsert = leftover.copy()
+          toInsert[denom] += 1
+          cache[amt] = toInsert
+    result = cache[amount]
+    if result is not None:
+      for denom in coins:
+        result[denom] = result[denom]
+      return result
+    else:
+      return None
 
 if __name__ == '__main__':
   soln = Solution()
-  coins = [3,5,7]
+  coins = { 1:10, 4:1, 5:5 }
   print(coins)
-  for amount in range(25):
-    result =  soln.coinChange3(coins, amount)
+  def maxValue(coinDict: Dict[int,int]):
+    sum = 0
+    for coin, value in coinDict.items():
+      sum += coin * value
+    return sum
+  for amount in range(maxValue(coins) + 2):
+    result =  soln.coinChange4(coins, amount)
     result = dict(result) if result is not None else None
     print("amount: ",amount,", coins needed: ",result,sep="")
 
