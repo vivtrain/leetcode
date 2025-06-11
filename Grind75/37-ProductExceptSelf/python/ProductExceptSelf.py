30e0b9fdc914a617eb91c1d3dfc47dbdfd0a29d3
Vivek Kumar <k99vivek@gmail.com>
2025-06-10 19:38:56 -0700

    Completed 37-ProductExceptSelf/python

--- Grind75/37-ProductExceptSelf/python/ProductExceptSelf.py
+++ Grind75/37-ProductExceptSelf/python/ProductExceptSelf.py
@@ -2,16 +2,45 @@
 
 from __future__ import annotations
 import sys
+
 sys.path.append('../../Utility/python')
+from typing import List
+from PrettyPrint import color
+from Colors import *
 
 class Solution(object):
-  pass
+  def productExceptSelf(self, nums: List[int]) -> List[int]:
+    N: int = len(nums)
+    if N < 2:
+      return []
+    prodExceptSelf: List[int] = [ 1 ] * N
+    for i in range(1, N, 1):
+      prodExceptSelf[i] = prodExceptSelf[i-1] * nums[i-1]
+    prodsBackward = 1
+    for i in range(N-1, -1, -1):
+      prodExceptSelf[i] *= prodsBackward
+      prodsBackward *= nums[i]
+    return prodExceptSelf
 
 if __name__ == '__main__':
   soln = Solution()
-  testCases = []
+  testCases = [
+    ([], []),
+    ([1], []),
+    ([1,2], [2,1]),
+    ([1,2,3], [6,3,2]),
+    ([-1,2,3], [6,-3,-2]),
+    ([0,2,3], [6,0,0]),
+    ([3,2,4,-1,-5], [40,60,30,-120,-24]),
+  ]
 
-  for test in testCases:
-    pass
-  print('TESTS PASSED!')
+  for nums, answer in testCases:
+    print(color("test:", LIGHT_BLUE), nums)
+    print("       =>", answer)
+    result = soln.productExceptSelf(nums)
+    print(color("       ->", LIGHT_YELLOW), result, end=' ')
+    if result == answer:
+      print(color("PASS", GREEN))
+    else:
+      print(color("FAIL", RED))
 
