92e903d3bfaf1b42cbe154dac2727127920cf9ee
Vivek Kumar <k99vivek@gmail.com>
2025-06-12 16:11:45 -0700

    Completed 39-ValidateBST/python

--- Grind75/39-ValidateBST/python/ValidateBST.py
+++ Grind75/39-ValidateBST/python/ValidateBST.py
@@ -3,14 +3,47 @@
 from __future__ import annotations
 import sys
 sys.path.append('../../Utility/python')
+from typing import Optional
+from Tree import *
+from math import inf
 
-class Solution(object):
-  pass
+class Solution:
+  def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    if root is None:
+      return True
+    def isValid(node: Optional[TreeNode],
+                lowerBound: Optional[int],
+                upperBound: Optional[int]) -> bool:
+      if node is None:
+        return True
+      if lowerBound is not None and node.val <= lowerBound:
+        return False
+      if upperBound is not None and node.val >= upperBound:
+        return False
+      return isValid(node.left, lowerBound, node.val) \
+          and isValid(node.right, node.val, upperBound)
+    return isValid(root.left, None, root.val) \
+        and isValid(root.right, root.val, None)
+
+  def isValidBST2(self, root: Optional[TreeNode]) -> bool:
+    if root is None:
+      return True
+    prev = -inf
+    def isValid(node: Optional[TreeNode]) -> bool:
+      if node is None:
+        return True
+      if not isValid(node.left):
+        return False
+      nonlocal prev
+      if node.val <= prev:
+        return False
+      prev = node.val
+      if not isValid(node.right):
+        return False
+      return True
+    return isValid(root)
+
 
 if __name__ == '__main__':
   soln = Solution()
-  testCases = []
-
-  for test in testCases:
-    pass
 
