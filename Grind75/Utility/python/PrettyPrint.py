ff55f68868ceabcbf0bded740399747ce88f0b2c
Vivek Kumar <k99vivek@gmail.com>
2025-06-17 13:22:03 -0700

    New python colorPrint

--- Grind75/Utility/python/PrettyPrint.py
+++ Grind75/Utility/python/PrettyPrint.py
@@ -4,6 +4,16 @@ import Colors
 def color(input: Any, color: str) -> str:
   return color + str(input) + Colors.NO_COLOR
 
+def colorPrint(color: str, *args, **kwargs):
+  if 'sep' not in kwargs:
+    kwargs['sep'] = ' '
+  if 'end' not in kwargs:
+    kwargs['end'] = '\n'
+  print(color, end='')
+  for a in args:
+    print(a, end=kwargs['sep'])
+  print(Colors.NO_COLOR, end=kwargs['end'])
+
 def prettyPrint2D(mat: List[List[Any]]) -> None:
   print('[[' + ']\n ['.join([', '.join([str(cell) for cell in row]) for row in mat]) + ']]')
 
