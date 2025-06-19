eb4181b5353cf8d64f0e86a95ea8e41d09638d08
Vivek Kumar <k99vivek@gmail.com>
2025-06-18 19:17:06 -0700

    Optimize memory for 77-Combinations/python

--- problems/77-Combinations/python/Combinations.py
+++ problems/77-Combinations/python/Combinations.py
@@ -12,20 +12,38 @@ class Solution(object):
     combinations: List[List[int]] = []
     comb: List[int] = []
 
+    def combine(nLeft: int, kLeft: int):
+      if nLeft < kLeft:
+        return
+      if kLeft == 0:
+        combinations.append(comb[:])
+        return
+      # take
+      comb.append(nLeft)
+      combine(nLeft-1, kLeft-1)
+      comb.pop()
+      # skip
+      combine(nLeft-1, kLeft)
+
+    combine(n, k)
+    return combinations
+
+  def combine2(self, n: int, k: int) -> List[List[int]]:
+    combinations: List[List[int]] = []
+    comb: List[int] = []
+
     def combine(nums: List[int], leftToChoose: int):
       if len(nums) < leftToChoose:
         return
       if leftToChoose == 0:
         combinations.append(comb[:])
         return
-      # take
       n = nums.pop()
+      # take
       comb.append(n)
       combine(nums, leftToChoose-1)
       comb.pop()
-      nums.append(n)
       # skip
-      n = nums.pop()
       combine(nums, leftToChoose)
       nums.append(n)
 
