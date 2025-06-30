c2e3fba7d98787b97671ca0ccce7d9788762d8c3
Vivek Kumar <k99vivek@gmail.com>
2025-06-29 17:39:06 -0700

    Add PrettyPrint for other stl containers

--- Grind75/Utility/cpp/include/impl/test.cpp
+++ Grind75/Utility/cpp/include/impl/test.cpp
@@ -1,37 +1,70 @@
 #include <colors.h>
+#include <map>
 #include <ostream>
 #include <prettyPrint.h>
 #include <vector>
 #include <iostream>
 
-int main() {
-  std::vector<int> vec;
-  std::vector<std::vector<int>> vec2D;
+using namespace std;
+
+void main2() {
+  vector<int> vec;
+  vector<vector<int>> vec2D;
 
   prettyPrint(vec);
-  std::cout << prettyString(vec) << std::endl;
+  cout << prettyString(vec) << endl;
   prettyPrint2D(vec2D);
   vec.push_back(1);
   vec2D.push_back(vec);
-  std::cout << std::endl;
+  cout << endl;
 
   prettyPrint(vec);
-  std::cout << prettyString(vec) << std::endl;
+  cout << prettyString(vec) << endl;
   prettyPrint2D(vec2D);
   vec.push_back(2);
   vec2D.push_back(vec);
-  std::cout << std::endl;
+  cout << endl;
 
   prettyPrint(vec);
-  std::cout << prettyString(vec) << std::endl;
+  cout << prettyString(vec) << endl;
   prettyPrint2D(vec2D);
   vec.push_back(3);
   vec2D.push_back(vec);
-  std::cout << std::endl;
+  cout << endl;
 
   prettyPrint(vec);
-  std::cout << prettyString(vec) << std::endl;
-  colorOut(colors::BLUE)<< prettyString(vec) << std::endl;
+  cout << prettyString(vec) << endl;
+  colorOut(colors::BLUE, prettyString(vec)) << endl;
   prettyPrint2D(vec2D);
 }
 
+int main() {
+  map<int, string> m;
+  unordered_map<string, int> um;
+  set<int> s;
+  unordered_set<string> us;
+  prettyPrint(m);
+  prettyPrint(um);
+  prettyPrint(s);
+  prettyPrint(us);
+  m[1] = "m1";
+  um["um1"] = 1;
+  s.insert(1);
+  us.insert("s1");
+  prettyPrint(m);
+  prettyPrint(um);
+  prettyPrint(s);
+  prettyPrint(us);
+  m[2] = "m2";
+  um["um2"] = 2;
+  s.insert(2);
+  us.insert("s2");
+  prettyPrint(m);
+  prettyPrint(um);
+  prettyPrint(s);
+  prettyPrint(us);
+  colorOut(colors::LIGHT_CYAN, prettyString(m)) << endl;
+  colorOut(colors::LIGHT_BLUE, prettyString(um, ",", ":")) << endl;
+  colorOut(colors::LIGHT_GREEN, prettyString(s)) << endl;
+  colorOut(colors::LIGHT_PURPLE, prettyString(us, " ")) << endl;
+}
--- Grind75/Utility/cpp/include/prettyPrint.h
+++ Grind75/Utility/cpp/include/prettyPrint.h
@@ -2,38 +2,160 @@
 #define PRETTY_PRINT
 
 #include <ostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+#include <set>
 #include <vector>
 #include <string>
 #include <sstream>
 #include <iostream>
 #include "colors.h"
 
+// PRETTY PRINT ╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍
 template<typename T>
 void prettyPrint(const std::vector<T>& vec, std::string delim=", ") {
   std::cout << '[';
   if (vec.size() > 0) {
-    for (auto it = vec.begin(); it != vec.end()-1; it++)
+    auto it = vec.begin();
+    for (auto itAhead = ++vec.begin(); itAhead != vec.end(); it++, itAhead++)
       std::cout << *it << delim;
-    std::cout << *(vec.end()-1);
+    std::cout << *it;
   }
   std::cout << ']' << std::endl;
 }
+template<typename T>
+void prettyPrint(const std::set<T> &set, std::string delim=", ") {
+  std::cout << '{';
+  if (set.size() > 0) {
+    auto it = set.begin();
+    for (auto itAhead = ++set.begin(); itAhead != set.end(); it++, itAhead++)
+      std::cout << *it << delim;
+    std::cout << *it;
+  }
+  std::cout << '}' << std::endl;
+}
+template<typename T>
+void prettyPrint(const std::unordered_set<T> &uset, std::string delim=", ") {
+  std::cout << '{';
+  if (uset.size() > 0) {
+    auto it = uset.begin();
+    for (auto itAhead = ++uset.begin(); itAhead != uset.end(); it++, itAhead++)
+      std::cout << *it << delim;
+    std::cout << *it;
+  }
+  std::cout << '}' << std::endl;
+}
+template<typename T1, typename T2>
+void prettyPrint(
+    const std::map<T1, T2> &map,
+    std::string delim=", ",
+    std::string betw=": ") {
+  std::cout << '{';
+  if (map.size() > 0) {
+    auto it = map.begin();
+    for (auto itAhead = ++map.begin(); itAhead != map.end(); it++, itAhead++)
+      std::cout << it->first << betw << it->second << delim;
+    std::cout << it->first << betw << it->second;
+  }
+  std::cout << '}' << std::endl;
+}
+template<typename T1, typename T2>
+void prettyPrint(
+    const std::unordered_map<T1, T2> &umap,
+    std::string delim=", ",
+    std::string betw=": ") {
+  std::cout << '{';
+  if (umap.size() > 0) {
+    auto it = umap.begin();
+    for (auto itAhead = ++umap.begin(); itAhead != umap.end(); it++, itAhead++)
+      std::cout << it->first << betw << it->second << delim;
+    std::cout << it->first << betw << it->second;
+  }
+  std::cout << '}' << std::endl;
+}
 
+// PRETTY STRING ╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍
 template<typename T>
 std::string prettyString(const std::vector<T>& vec, std::string delim=", ") {
   std::stringstream output;
   output << '[';
   if (vec.size() > 0) {
-    for (auto it = vec.begin(); it != vec.end()-1; it++)
+    auto it = vec.begin();
+    for (auto itAhead = ++vec.begin(); itAhead != vec.end(); it++, itAhead++)
       output << *it << delim;
-    output << *(vec.end()-1);
+    output << *it;
   }
   output << ']';
   return output.str();
 }
+template<typename T>
+std::string prettyString(const std::set<T>& set, std::string delim=", ") {
+  std::stringstream output;
+  output << '{';
+  if (set.size() > 0) {
+    auto it = set.begin();
+    for (auto itAhead = ++set.begin(); itAhead != set.end(); it++, itAhead++)
+      output << *it << delim;
+    output << *it;
+  }
+  output << '}';
+  return output.str();
+}
+template<typename T>
+std::string prettyString(
+    const std::unordered_set<T>& uset,
+    std::string delim=", ") {
+  std::stringstream output;
+  output << '{';
+  if (uset.size() > 0) {
+    auto it = uset.begin();
+    for (auto itAhead = ++uset.begin(); itAhead != uset.end(); it++, itAhead++)
+      output << *it << delim;
+    output << *it;
+  }
+  output << '}';
+  return output.str();
+}
+template<typename T1, typename T2>
+std::string prettyString(
+    const std::map<T1, T2>& map,
+    std::string delim=", ",
+    std::string betw=": ") {
+  std::stringstream output;
+  output << '{';
+  if (map.size() > 0) {
+    auto it = map.begin();
+    for (auto itAhead = ++map.begin(); itAhead != map.end(); it++, itAhead++)
+      output << it->first << betw << it->second << delim;
+    output << it->first << betw << it->second;
+  }
+  output << '}';
+  return output.str();
+}
+template<typename T1, typename T2>
+std::string prettyString(
+    const std::unordered_map<T1, T2>& umap,
+    std::string delim=", ",
+    std::string betw=": ") {
+  std::stringstream output;
+  output << '{';
+  if (umap.size() > 0) {
+    auto it = umap.begin();
+    for (auto itAhead = ++umap.begin(); itAhead != umap.end(); it++, itAhead++)
+      output << it->first << betw << it->second << delim;
+    output << it->first << betw << it->second;
+  }
+  output << '}';
+  return output.str();
+}
 
+// PRETTY PRINT 2D ╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍
 template<class T>
-void prettyPrint2D(const std::vector<std::vector<T>>& vec, std::string outerDelim="\n ", std::string innerDelim=", ") {
+void prettyPrint2D(
+    const std::vector<std::vector<T>>& vec,
+    std::string outerDelim="\n",
+    std::string innerDelim=", ") {
   std::cout << "[";
   if (vec.size() > 0)
     std::cout << prettyString(*vec.begin(), innerDelim);
@@ -47,6 +169,7 @@ void prettyPrint2D(const std::vector<std::vector<T>>& vec, std::string outerDeli
   std::cout << "]" << std::endl;
 }
 
+// PRETTY STRING 2D ╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍
 template<class T>
 std::string prettyString2D(const std::vector<std::vector<T>>& vec, std::string outerDelim="\n", std::string innerDelim=", ") {
   std::stringstream output;
