a33147a4e793ad8017bd6bb0b034a0a0927d8283
Vivek Kumar <k99vivek@gmail.com>
2025-07-20 14:15:48 -0700

    Add heap solution to 70-FindMedianFromDataStream/cpp

--- Grind75/70-FindMedianFromDataStream/cpp/FindMedianFromDataStream.cpp
+++ Grind75/70-FindMedianFromDataStream/cpp/FindMedianFromDataStream.cpp
@@ -4,9 +4,42 @@
 
 using namespace std;
 
-MedianFinder::MedianFinder() : nums(), mid(nums.end()) {}
+// Min-Max Heaps Solution ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+
+MedianFinder::MedianFinder() : minHeap(), maxHeap() {}
 
 void MedianFinder::addNum(int num) {
+  if (num > findMedian())
+    minHeap.push(num);
+  else
+    maxHeap.push(num);
+
+  if (minHeap.size() > maxHeap.size() + 1) {
+    maxHeap.push(minHeap.top());
+    minHeap.pop();
+  } else if (maxHeap.size() > minHeap.size() + 1) {
+    minHeap.push(maxHeap.top());
+    maxHeap.pop();
+  }
+}
+
+double MedianFinder::findMedian() {
+  if (minHeap.empty() && maxHeap.empty())
+    return 0.0;
+  else if (minHeap.size() > maxHeap.size()) {
+    return minHeap.top();
+  } else if (minHeap.size() < maxHeap.size()) {
+    return maxHeap.top();
+  } else {
+    return (minHeap.top() + maxHeap.top()) / 2.0;
+  }
+}
+
+// Multiset Solution ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+
+MedianFinder2::MedianFinder2() : nums(), mid(nums.end()) {}
+
+void MedianFinder2::addNum(int num) {
   nums.insert(num);
   if (nums.size() == 1)
     mid = nums.begin();
@@ -16,7 +49,7 @@ void MedianFinder::addNum(int num) {
     mid++;
 }
 
-double MedianFinder::findMedian() {
+double MedianFinder2::findMedian() {
   if (mid == nums.end())
     return 0.0;
   return evenLength() ? (*mid + *(next(mid))) / 2.0 : *mid;
--- Grind75/70-FindMedianFromDataStream/cpp/FindMedianFromDataStream.h
+++ Grind75/70-FindMedianFromDataStream/cpp/FindMedianFromDataStream.h
@@ -1,10 +1,26 @@
 #ifndef FIND_MEDIAN_FROM_DATA_STREAM
 #define FIND_MEDIAN_FROM_DATA_STREAM
 
+#include <functional>
+#include <queue>
 #include <set>
+#include <vector>
+
+// Min-Max Heaps Solution ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 class MedianFinder {
   public:
     MedianFinder();
+    void addNum(int num);
+    double findMedian();
+  private:
+    std::priority_queue<int, std::vector<int>, std::greater<int>> minHeap;
+    std::priority_queue<int, std::vector<int>, std::less<int>> maxHeap;
+};
+
+// Multiset Solution ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+class MedianFinder2 {
+  public:
+    MedianFinder2();
     void addNum(int num);
     double findMedian();
   private:
